71c8b3ee9ee616da39ecfc64f31a7065b2c397aa
Onurcan Erşen <ocersen@gmail.com>
2023-05-16 17:04:42 +0300

    Add local dev address

--- src/app/app.settings.ts
+++ src/app/app.settings.ts
@@ -1,8 +1,10 @@
 export class AppSettings {
   public static get API_ENDPOINT(): string {
     //return 'http://metabolitics.biodb.sehir.edu.tr/api';
+    // Comment below line for local development
     return 'http://164.90.180.104/api';
-     // return 'http://127.0.0.1:5000';
+    // Uncomment below line for local development
+    //return 'http://127.0.0.1:5000';
   }
 
   public static get NOTIFICATION_OPTIONS() {
