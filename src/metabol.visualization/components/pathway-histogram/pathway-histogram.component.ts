521eda7ab38d23f53f529221fc8f39fd42ff1d70
Onurcan Erşen <ocersen@gmail.com>
2023-11-03 23:58:48 +0300

    Update pathway enrichment page

--- src/metabol.visualization/components/pathway-histogram/pathway-histogram.component.ts
+++ src/metabol.visualization/components/pathway-histogram/pathway-histogram.component.ts
@@ -23,6 +23,7 @@ export class PathwayHistogramComponent implements OnInit, AfterViewInit {
   }];
 
   @Input() pathwayScores: Array<object>;
+  @Input() method: String;
 
   ngOnInit() { }
 
@@ -32,7 +33,7 @@ export class PathwayHistogramComponent implements OnInit, AfterViewInit {
     this.data[0].y = [];
 
     for (let scores of this.pathwayScores) {
-      let sortedScores = _.orderBy(_.toPairs(scores), [(x) => Math.abs(x[1])], ['desc']).slice(0, 20);
+      let sortedScores = _.orderBy(_.toPairs(scores), [(x) => Math.abs(x[1])], [this.method === 'Pathway Enrichment' ? 'asc' : 'desc']).slice(0, 20);
       this.data[0].x = sortedScores.map(x => x[0]);
       this.data[0].y = sortedScores.map(x => x[1]);
       this.data[0].marker.color = sortedScores.map(x => x[1] > 0 ? '#3F51B5' : '#E91E63');
