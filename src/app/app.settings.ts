36687fe8567a7cfa048590173179367d97481abd
Onurcan Erşen <ocersen@gmail.com>
2023-11-14 01:25:23 +0300

    Update app.settings.ts

--- src/app/app.settings.ts
+++ src/app/app.settings.ts
@@ -2,9 +2,10 @@ export class AppSettings {
   public static get API_ENDPOINT(): string {
     //return 'http://metabolitics.biodb.sehir.edu.tr/api';
     // Comment below line for local development
-    return 'http://164.90.180.104/api';
+    // return 'http://164.90.180.104/api';
     // Uncomment below line for local development
     //return 'http://127.0.0.1:5000';
+    return 'http://metabolitics.itu.edu.tr/api';
   }
 
   public static get NOTIFICATION_OPTIONS() {
