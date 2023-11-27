eaeeeb4cebf7f5c180de421dfe8229310e8a4966
Onurcan Erşen <ocersen@gmail.com>
2023-11-28 02:41:41 +0300

    Update plot labels

--- src/metabol.visualization/components/pathway-histogram/pathway-histogram.component.ts
+++ src/metabol.visualization/components/pathway-histogram/pathway-histogram.component.ts
@@ -16,11 +16,22 @@ export class PathwayHistogramComponent implements OnInit, AfterViewInit {
     x: ['a', 'b', 'c'],
     y: [1, 20, 30],
     type: 'bar',
+    text: [],
+    textposition: 'auto',
+    hoverinfo: 'none',
     marker: {
       color: []
     },
 
   }];
+  layout = {
+    xaxis: {
+      automargin: true,
+    },
+    yaxis: {
+      automargin: true,
+    },
+  };
 
   @Input() pathwayScores: Array<object>;
   @Input() method: String;
@@ -36,9 +47,10 @@ export class PathwayHistogramComponent implements OnInit, AfterViewInit {
       let sortedScores = _.orderBy(_.toPairs(scores), [(x) => Math.abs(x[1])], [this.method === 'Pathway Enrichment' ? 'asc' : 'desc']).slice(0, 20);
       this.data[0].x = sortedScores.map(x => x[0]);
       this.data[0].y = sortedScores.map(x => x[1]);
+      this.data[0].text = sortedScores.map(x => x[1].toPrecision(3));
       this.data[0].marker.color = sortedScores.map(x => x[1] > 0 ? '#3F51B5' : '#E91E63');
     }
-    Plotly.react('histogram', this.data);
+    Plotly.react('histogram', this.data, this.layout);
   }
 
 }
